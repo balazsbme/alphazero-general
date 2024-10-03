bd96a607f4c821ccf8a58784114aee66a1eed62c
Balazs Nemeth <nem841425bal@gmail.com>
2024-10-03 22:18:57 +0200

    parameterize gobang pit script
    Example command:
     - python3 -m alphazero.envs.gobang.pit --checkpoint_folder checkpoint/gobang9x9/ --checkpoint mcts-100-iteration-0145.pkl --opponent_type human

--- alphazero/envs/gobang/pit.py
+++ alphazero/envs/gobang/pit.py
@@ -1,3 +1,5 @@
+import argparse
+import json
 from pyximport import install
 from numpy import get_include
 install(setup_args={'include_dirs': get_include()})
@@ -5,6 +7,7 @@ install(setup_args={'include_dirs': get_include()})
 from alphazero.Arena import Arena
 from alphazero.GenericPlayers import *
 from alphazero.NNetWrapper import NNetWrapper as NNet
+from alphazero.Coach import get_args
 
 
 """
@@ -17,8 +20,24 @@ if __name__ == '__main__':
     from alphazero.envs.gobang.GobangPlayers import HumanGobangPlayer
     import random
 
+    parser = argparse.ArgumentParser(description='Run the AlphaZero Gobang pit.')
+    parser.add_argument('--opponent_type', type=str, choices=['self', 'human'], default='self',
+                        help='Type of player: "self" for neural network, "human" for human player')
+    parser.add_argument('--checkpoint_folder', type=str, required=False,
+                        help='Full path to the first checkpoint folder')
+    parser.add_argument('--checkpoint', type=str, required=False,
+                        help='Filename of checkpoint')
+    parser.add_argument('--args_path', type=str, required=False,
+                    help='Path to the args.json file')
+    script_args = parser.parse_args()
+
+    if script_args.args_path is not None:
+        with open(script_args.args_path, "r") as f:
+            args = json.load(f)
+            args = get_args(args)
     batched_arena = False
-    args.numMCTSSims = 800
+    args.numMCTSSims = 400
+    args._num_players = 2
     #args.arena_batch_size = 64
     args.temp_scaling_fn = lambda x,y,z:0.2
     #args2.temp_scaling_fn = args.temp_scaling_fn
@@ -27,14 +46,25 @@ if __name__ == '__main__':
 
     # nnet players
     nn1 = NNet(Game, args)
-    nn1.load_checkpoint('./checkpoint/' + args.run_name, 'iteration-0101.pkl')
-    #nn2 = NNet(Game, args)
-    #nn2.load_checkpoint('./checkpoint/brandubh2', 'iteration-0112.pkl')
+    if script_args.checkpoint_folder:
+        nn1.load_checkpoint(script_args.checkpoint_folder, script_args.checkpoint)
+    else:
+        nn1.load_checkpoint('./checkpoint/' + args.run_name, 'mcts-100-iteration-0145.pkl')
+    player1 = NNPlayer(nn1, args=args, verbose=True)
+
+    if script_args.opponent_type == 'self':
+        nn2 = NNet(Game, args)
+        if script_args.checkpoint_folder:
+            nn2.load_checkpoint(script_args.checkpoint_folder, script_args.checkpoint)
+        else:
+            nn2.load_checkpoint('./checkpoint/' + args.run_name, 'iteration-0118.pkl')
+        player2 = NNPlayer(nn2, args=args, verbose=True)
+    elif script_args.opponent_type == 'human':
+        player2 = HumanGobangPlayer(Game, args=args, verbose=True)
+
     #player1 = nn1.process
     #player2 = nn2.process
 
-    player1 = NNPlayer(nn1, args=args, verbose=True)
-    player2 = HumanGobangPlayer(Game, args=args, verbose=True)
     #player2 = RandomPlayer()
     #player2 = GreedyTaflPlayer()
     #player2 = RandomPlayer()
